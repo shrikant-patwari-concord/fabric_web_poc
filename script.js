fd8f6b21a4cd4dd522ddb25feeaa0c253588392c
shrikant-patwari-concord <125991186+shrikant-patwari-concord@users.noreply.github.com>
2023-03-30 11:57:41 +0530

    1 file modified, 1 added

--- script.js
+++ script.js
@@ -1492,31 +1492,31 @@ const jsonData = {
   },
 };
 
-jsonData.variables.template_data.faces.forEach(async (face, index) => {
-  const finalJson = await loadLayer(face, index + 1, false);
-  if (index == 0) {
-    const fcanvas = new fabric.Canvas(
-      document.querySelector('#fCanvas'),
-      face.dimensions
-    );
-    console.log(finalJson);
-    fcanvas.loadFromJSON(finalJson.CanvasJson, () => {
-      console.log(fcanvas);
-      fcanvas.renderAll.bind(fcanvas);
-    });
-  }
-  if (index == 1) {
-    const icanvas = new fabric.Canvas(
-      document.querySelector('#iCanvas'),
-      face.dimensions
-    );
-    console.log(finalJson);
-    icanvas.loadFromJSON(finalJson.CanvasJson, () => {
-      console.log(icanvas);
-      icanvas.renderAll.bind(icanvas);
-    });
-  }
-});
+// jsonData.variables.template_data.faces.forEach(async (face, index) => {
+//   const finalJson = await loadLayer(face, index + 1, false);
+//   if (index == 0) {
+//     const fcanvas = new fabric.Canvas(
+//       document.querySelector('#fCanvas'),
+//       face.dimensions
+//     );
+//     console.log(finalJson);
+//     fcanvas.loadFromJSON(finalJson.CanvasJson, () => {
+//       console.log(fcanvas);
+//       fcanvas.renderAll.bind(fcanvas);
+//     });
+//   }
+//   if (index == 1) {
+//     const icanvas = new fabric.Canvas(
+//       document.querySelector('#iCanvas'),
+//       face.dimensions
+//     );
+//     console.log(finalJson);
+//     icanvas.loadFromJSON(finalJson.CanvasJson, () => {
+//       console.log(icanvas);
+//       icanvas.renderAll.bind(icanvas);
+//     });
+//   }
+// });
 
 var initialData = {
   project_id: '0dfc54d9-5144-4e6b-ab8b-2508bc7a3850',
@@ -6184,3 +6184,520 @@ var initialData = {
     },
   },
 };
+
+const initialProjectData = {
+  project_id: '7e9d85ca-3c12-420b-9e3e-ff08a0980c03',
+  account_id: '2125445574',
+  name: 'POD Project',
+  product_id: '2PGM1207',
+  scan_code: '0002391359',
+  version: 1,
+  is_digital_fulfillment: false,
+  expiration_date: '2023-04-05T13:49:19.013821013Z',
+  project_type_code: 'P',
+  project_status_code: 'C',
+  created_at: '2023-03-29T13:49:19.013847831Z',
+  last_updated_at: '2023-03-29T13:49:19.013848884Z',
+  font_collection: {
+    default_size: 55,
+    default_color: '#000000',
+    fonts: [
+      {
+        id: 101,
+        name: 'Simply Yours',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/108317.ttf',
+      },
+      {
+        id: 102,
+        name: 'Grateful for You',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/126056.ttf',
+      },
+      {
+        id: 103,
+        name: 'Warmest Wishes',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/BerdingScript.ttf',
+      },
+      {
+        id: 104,
+        name: 'Yours Always',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/TuesdayHMK-MGE.ttf',
+      },
+      {
+        id: 105,
+        name: 'All My Best',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/KrickHMK-Regular.ttf',
+      },
+      {
+        id: 106,
+        name: 'Take It Easy',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/JohnsonBallpointPen.ttf',
+      },
+      {
+        id: 107,
+        name: 'Hey Sunshine',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/AnnettePrintMGE-Regular.ttf',
+      },
+      {
+        id: 108,
+        name: 'Stay Strong',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/JasonPrint.ttf',
+      },
+      {
+        id: 109,
+        name: "'Til Next Time",
+        url: 'https://content.dev.hallmark.com/POD_Fonts/126059.ttf',
+      },
+      {
+        id: 110,
+        name: 'Catch You Later',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/JohnsonPrint.ttf',
+      },
+      {
+        id: 111,
+        name: 'Keep in Touch',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/JenniferPrintLight.ttf',
+      },
+      {
+        id: 112,
+        name: 'Hugs to You',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/BrentPrint.ttf',
+      },
+      {
+        id: 113,
+        name: 'Kind Regards',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/TypewriteWornOneHMK.ttf',
+      },
+      {
+        id: 114,
+        name: 'Buh-Bye',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/AmbergerSansTextA.ttf',
+      },
+      {
+        id: 115,
+        name: 'Cheers to You',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/BeamNewHMK-Regular.ttf',
+      },
+      {
+        id: 116,
+        name: 'Later Gator',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/CrayottBookKB.ttf',
+      },
+      {
+        id: 117,
+        name: 'WHAT’S UP',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/AlmondMilkHMK-Regular.ttf',
+      },
+      {
+        id: 119,
+        name: 'Just Saying',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/SarahndipityHMK-Smooth.ttf',
+      },
+      {
+        id: 120,
+        name: 'OMG Hi',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/BeamNewHMK-Bold.ttf',
+      },
+      {
+        id: 121,
+        name: "How Ya Doin'",
+        url: 'https://content.dev.hallmark.com/POD_Fonts/HelloOne-HMK.ttf',
+      },
+      {
+        id: 122,
+        name: 'Just a Note',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/AstaSlabHMK-Medium.ttf',
+      },
+      {
+        id: 123,
+        name: 'Keep Smiling',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/MiziletteHMK-SemiBoldUpright.ttf',
+      },
+      {
+        id: 124,
+        name: 'Sincerely',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/QueensHatHMK-Italic.ttf',
+      },
+      {
+        id: 125,
+        name: 'Hiya Pal',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/MichaelaVFHMK.ttf',
+      },
+      {
+        id: 126,
+        name: 'Be Seeing You',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/FieldnotesHMK-Rough.ttf',
+      },
+      {
+        id: 127,
+        name: 'Good Vibes',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/GretaHMK-Regular.ttf',
+      },
+      {
+        id: 128,
+        name: 'Best Wishes',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/BernhardFashionOnePKA.ttf',
+      },
+      {
+        id: 129,
+        name: 'Hang Loose',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/RittenPrintLowRiseHMK-Regular.ttf',
+      },
+      {
+        id: 130,
+        name: 'Much Appreciated',
+        url: 'https://content.dev.hallmark.com/POD_Fonts/BethelHMK-Regular.ttf',
+      },
+    ],
+  },
+  product: {
+    product_id: '2PGM1207',
+    template_id: 'PGM1207',
+    product_name: 'Personalized Full Photo Birthday Photo Card, 5x7 Vertical',
+    vendor_lead_time: 1,
+    envelope_color: '#FFFFF',
+  },
+  fulfillment: {},
+  variables: {
+    template_data: {
+      CardFormat: 'portrait',
+      CardSize: '49',
+      CardType: 'photo',
+      Dimensions: {
+        Height: 179,
+        Width: 125,
+      },
+      Faces: [
+        {
+          BackgroundUrl:
+            'https://content.dev.hallmark.com/webassets/PGM1207/PGM1207_P1_Background.png',
+          CanvasJson: null,
+          Dimensions: {
+            Height: 2114,
+            Width: 1476,
+          },
+          EditableAreas: [],
+          FaceId: 1,
+          FrameUrl:
+            'https://content.dev.hallmark.com/webassets/PGM1207/PGM1207_P1_Frame.png',
+          IsEditable: true,
+          OverlayBackgroundUrl: '',
+          PhotoZones: [
+            {
+              Height: 1951.7098,
+              LeftPosition: 21.259802,
+              Rotation: 0,
+              TopPosition: 45.70975,
+              Width: 1363.6118,
+            },
+          ],
+          PreviewUrl:
+            'https://content.dev.hallmark.com/webassets/PGM1207/PGM1207_P1_Preview.png',
+          ReplaceBackgroundUrl: '',
+          Texts: [
+            {
+              FontFamily: 'OMG Hi',
+              FontId: 120,
+              FontSize: 26,
+              Height: 184.72404,
+              IsFixed: true,
+              IsHybrid: false,
+              IsMultiline: false,
+              LeftPosition: 170.5662,
+              Rotation: 0,
+              Text: 'RYLEIGH',
+              TextAlign: 'center',
+              TextColor: '#FFFFFF',
+              TopPosition: 1693.4612,
+              Width: 1063.9987,
+            },
+          ],
+          Type: 'front',
+          UserImages: null,
+          UserTextZones: [],
+        },
+        {
+          BackgroundUrl:
+            'https://content.dev.hallmark.com/webassets/PGM1207/PGM1207_P2-3_Background.png',
+          CanvasJson: null,
+          Dimensions: {
+            Height: 2114,
+            Width: 2870,
+          },
+          EditableAreas: [],
+          FaceId: 2,
+          FrameUrl: '',
+          IsEditable: true,
+          OverlayBackgroundUrl: '',
+          PhotoZones: [],
+          PreviewUrl:
+            'https://content.dev.hallmark.com/webassets/PGM1207/PGM1207_P2-3_Preview.png',
+          ReplaceBackgroundUrl: '',
+          Texts: [],
+          Type: 'inside',
+          UserImages: null,
+          UserTextZones: [],
+        },
+        {
+          BackgroundUrl:
+            'https://content.dev.hallmark.com/webassets/PGM1207/PGM1207_P4_Background.png',
+          CanvasJson: null,
+          Dimensions: {
+            Height: 2114,
+            Width: 1394,
+          },
+          EditableAreas: [],
+          FaceId: 3,
+          FrameUrl: '',
+          IsEditable: false,
+          OverlayBackgroundUrl: '',
+          PhotoZones: [],
+          PreviewUrl:
+            'https://content.dev.hallmark.com/webassets/PGM1207/PGM1207_P4_Preview.png',
+          ReplaceBackgroundUrl: '',
+          Texts: [],
+          Type: 'back',
+          UserImages: null,
+          UserTextZones: [],
+        },
+      ],
+      Name: 'PGM1207',
+      OpenOrientation: 'right',
+      ParentDimensions: {
+        Height: 179,
+        Width: 125,
+      },
+    },
+  },
+};
+
+const generateCanvasJSONUtil = (function () {
+  const projectObj = { personalization: [] };
+  function initializeProject(initialData) {
+    if (
+      initialData &&
+      initialData.variables &&
+      initialData.variables.template_data &&
+      initialData.variables.template_data.Faces &&
+      Array.isArray(initialData.variables.template_data.Faces)
+    ) {
+      Object.assign(projectObj, {
+        project_id: initialData.project_id,
+        account_id: initialData.account_id,
+        name: initialData.name,
+        product_id: initialData.product_id,
+        scan_code: initialData.scan_code,
+        version: initialData.version,
+        is_digital_fulfillment: initialData.is_digital_fulfillment,
+        expiration_date: initialData.expiration_date,
+        project_type_code: initialData.project_type_code,
+        project_status_code: initialData.project_status_code,
+        created_at: initialData.created_at,
+      });
+      initialData.variables.template_data.Faces.forEach((face, faceindex) => {
+        const personalizedFace = {
+          FaceId: face.FaceId,
+          FaceNumber: face.FaceId,
+          CanvasJson: {
+            version: '5.2.1',
+            objects: [],
+          },
+          UserImages: [],
+        };
+        if (face.BackgroundUrl) {
+          personalizedFace.CanvasJson.backgroundImage = {
+            type: 'image',
+            version: '5.2.1',
+            originX: 'left',
+            originY: 'top',
+            left: 0,
+            top: 0,
+            width: face.Dimensions.Width,
+            height: face.Dimensions.Height,
+            fill: 'transparent',
+            stroke: null,
+            strokeWidth: 0,
+            strokeDashArray: null,
+            strokeLineCap: 'butt',
+            strokeDashOffset: 0,
+            strokeLineJoin: 'miter',
+            strokeUniform: false,
+            strokeMiterLimit: 4,
+            scaleX: 1,
+            scaleY: 1,
+            angle: 0,
+            flipX: false,
+            flipY: false,
+            opacity: 1,
+            shadow: null,
+            visible: false,
+            backgroundColor: '',
+            fillRule: 'nonzero',
+            paintFirst: 'fill',
+            globalCompositeOperation: 'source-over',
+            skewX: 0,
+            skewY: 0,
+            cropX: 0,
+            cropY: 0,
+            src: face.BackgroundUrl,
+            crossOrigin: 'anonymous',
+            filters: [],
+          };
+        }
+        if (face.PhotoZones && Array.isArray(face.PhotoZones)) {
+          face.PhotoZones.forEach((photoZone, photoZoneIndex) => {
+            const photoRect = {
+              type: 'rect',
+              version: '5.2.1',
+              originX: 'left',
+              originY: 'top',
+              left: photoZone.LeftPosition,
+              top: photoZone.TopPosition,
+              width: photoZone.Width,
+              height: photoZone.Height,
+              fill: '#838684',
+              stroke: null,
+              strokeWidth: 0,
+              strokeDashArray: null,
+              strokeLineCap: 'butt',
+              strokeDashOffset: 0,
+              strokeLineJoin: 'miter',
+              strokeUniform: false,
+              strokeMiterLimit: 4,
+              scaleX: 1,
+              scaleY: 1,
+              angle: photoZone.Rotation || 0,
+              flipX: false,
+              flipY: false,
+              opacity: 1,
+              shadow: null,
+              visible: true,
+              backgroundColor: '',
+              fillRule: 'nonzero',
+              paintFirst: 'fill',
+              globalCompositeOperation: 'source-over',
+              skewX: 0,
+              skewY: 0,
+              rx: 0,
+              ry: 0,
+              name: `photozone-${photoZoneIndex}`,
+            };
+            personalizedFace.CanvasJson.objects.push(photoRect);
+          });
+        }
+        if (face.FrameUrl) {
+          const frameImageObj = {
+            type: 'image',
+            version: '5.2.1',
+            originX: 'left',
+            originY: 'top',
+            left: 0,
+            top: 0,
+            width: face.Dimensions.Width,
+            height: face.Dimensions.Height,
+            fill: 'rgb(0,0,0)',
+            stroke: null,
+            strokeWidth: 0,
+            strokeDashArray: null,
+            strokeLineCap: 'butt',
+            strokeDashOffset: 0,
+            strokeLineJoin: 'miter',
+            strokeUniform: false,
+            strokeMiterLimit: 4,
+            scaleX: 1,
+            scaleY: 1,
+            angle: 0,
+            flipX: false,
+            flipY: false,
+            opacity: 1,
+            shadow: null,
+            visible: true,
+            backgroundColor: '',
+            fillRule: 'nonzero',
+            paintFirst: 'fill',
+            globalCompositeOperation: 'source-over',
+            skewX: 0,
+            skewY: 0,
+            cropX: 0,
+            cropY: 0,
+            name: 'overlayImg',
+            src: face.FrameUrl,
+            crossOrigin: 'anonymous',
+            filters: [],
+          };
+          personalizedFace.CanvasJson.objects.push(frameImageObj);
+        }
+        if (face.Texts && Array.isArray(face.Texts)) {
+          face.Texts.forEach((text, textIndex) => {
+            const textObj = {
+              type: 'textbox',
+              version: '5.2.1',
+              originX: 'left',
+              originY: 'top',
+              left: text.LeftPosition,
+              top: text.TopPosition,
+              width: text.Width,
+              height: text.Height,
+              fill: text.TextColor,
+              stroke: null,
+              strokeWidth: 1,
+              strokeDashArray: null,
+              strokeLineCap: 'butt',
+              strokeDashOffset: 0,
+              strokeLineJoin: 'miter',
+              strokeUniform: false,
+              strokeMiterLimit: 4,
+              scaleX: 1,
+              scaleY: 1,
+              angle: 0,
+              flipX: false,
+              flipY: false,
+              opacity: 1,
+              shadow: null,
+              visible: true,
+              backgroundColor: 'transparent',
+              fillRule: 'nonzero',
+              paintFirst: 'fill',
+              globalCompositeOperation: 'source-over',
+              skewX: 0,
+              skewY: 0,
+              fontFamily: `fontid-${text.FontId}`,
+              fontWeight: 'normal',
+              fontSize: text.FontSize * 4,
+              text: text.Text,
+              underline: false,
+              overline: false,
+              linethrough: false,
+              textAlign: text.TextAlign,
+              fontStyle: 'normal',
+              lineHeight: 1.16,
+              textBackgroundColor: '',
+              charSpacing: 0,
+              styles: {},
+              direction: 'ltr',
+              path: null,
+              pathStartOffset: 0,
+              pathSide: 'left',
+              pathAlign: 'baseline',
+              minWidth: 20,
+              splitByGrapheme: false,
+              name: `userTextbox-${face.FaceId}-${textIndex}`,
+            };
+            personalizedFace.CanvasJson.objects.push(textObj);
+          });
+        }
+        projectObj.personalization.push(personalizedFace);
+      });
+    }
+    function getProjectData() {}
+  }
+  function getProjectData() {
+    return projectObj;
+  }
+  return {
+    initializeProject,
+    getProjectData,
+  };
+})();
+
+generateCanvasJSONUtil.initializeProject(initialProjectData);
+console.log(generateCanvasJSONUtil.getProjectData());
